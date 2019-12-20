ed258b5693a1be1d592cf9f6b3eb1ffe7dc4155a
Johnnek <stylano@gmail.com>
2019-12-20 20:30:45 +0100

    change gitignore

---
+++ src/main/java/utils/Rules.java
@@ -0,0 +1,15 @@
+package utils;
+
+import models.Player;
+
+public class Rules {
+
+    /**
+     Paper wins against Rock
+     Rock wins against Scissor
+     Scissor wins against Paper
+     */
+    public int checkWinner(Player playerA, Player playerB){
+        return 0;
+    }
+}
