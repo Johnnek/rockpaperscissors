f8c1fc473527e1f5a45ae6478d53827d11e1164c
Johnnek <stylano@gmail.com>
2019-12-20 20:19:37 +0100

    Create class to count the wins

---
+++ src/main/java/models/WinCounter.java
@@ -0,0 +1,32 @@
+package models;
+
+public class WinCounter {
+
+    private int draws;
+    private int winnerPlayerA;
+    private int winnerPlayerB;
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public void setDraws(int draws) {
+        this.draws = draws;
+    }
+
+    public int getWinnerPlayerA() {
+        return winnerPlayerA;
+    }
+
+    public void setWinnerPlayerA(int winnerPlayerA) {
+        this.winnerPlayerA = winnerPlayerA;
+    }
+
+    public int getWinnerPlayerB() {
+        return winnerPlayerB;
+    }
+
+    public void setWinnerPlayerB(int winnerPlayerB) {
+        this.winnerPlayerB = winnerPlayerB;
+    }
+}
