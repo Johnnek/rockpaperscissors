a8cc500164cb9b9d50c3dbb6b7b47553aea4cd8a
Johnnek <stylano@gmail.com>
2019-12-20 20:17:47 +0100

    add params to function prototypes, remove counter prototype, change return type in faceOff prototype

--- src/main/java/services/Game.java
+++ src/main/java/services/Game.java
@@ -1,14 +1,13 @@
 package services;
 
 import models.Player;
+import models.WinCounter;
 
 public interface Game {
 
     void runGame();
 
-    Player chooseTurn(Player player);
+    Player chooseTurn(Player player, WinCounter winCounter);
 
-    Player faceOff(Player playerA, Player playerB);
-
-    int counter();
+    int faceOff(Player playerA, Player playerB);
 }
