8143a50f3889428ed8b0cdae6cf299baa54925a9
Johnnek <stylano@gmail.com>
2019-12-26 22:44:52 +0100

    add output, change while condition

--- src/main/java/RockPaperScissorsApplication.java
+++ src/main/java/RockPaperScissorsApplication.java
@@ -8,24 +8,25 @@ class RockPaperScissorsApplication {
     public static void main(String[] args) {
         GameService gameService = new GameServiceImpl();
         int input = 0;
-
-        System.out.println("Do you want to start a game with a length of 100 rounds or variable length?");
+        System.out.println("This application starts a Rock - Paper - Scissors game between two players.");
+        System.out.println("You have to choose between two game modes. A game with a round length of 100 " +
+                "or a game with variable round length. Do you want to start a game with a length of 100 rounds " +
+                "or variable length?");
         System.out.print("Type in 1 for 100 rounds or 2 for variable rounds: ");
 
-        while (input == 0 || input > 2) {
+        // while (input != 1 && input != 2)
+        while (!(input > 0 && input < 3)) {
             Scanner modeScanner = new Scanner(System.in);
 
             input = modeScanner.nextInt();
 
             if (input == 1) {
                 gameService.runGame(100);
-                break;
             } else if (input == 2) {
                 System.out.println("How many rounds do you want to play?");
                 Scanner roundScanner = new Scanner(System.in);
                 int maxRounds = roundScanner.nextInt();
                 gameService.runGame(maxRounds);
-                break;
             } else System.out.print("Wrong input. Please choose between 1 or 2: ");
         }
     }
